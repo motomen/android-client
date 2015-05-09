2be371322760951d8fabb06c4e47e09044bff5a6
motomen <gdpdp.ru@mail.ru>
2015-05-09 14:29:38 +0300

    fix some bugs

--- searchprogm/app/src/main/java/com/application/yaroslav/searchprogm/NotFoundActivity.java
+++ searchprogm/app/src/main/java/com/application/yaroslav/searchprogm/NotFoundActivity.java
@@ -1,7 +1,9 @@
 package com.application.yaroslav.searchprogm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Yaroslav on 09.05.2015.
@@ -13,4 +15,14 @@ public class NotFoundActivity extends Activity {
         setContentView(R.layout.not_found);
     }
 
+    public void back(View v) {
+        switch (v.getId()) {
+            case R.id.back_button:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 }
